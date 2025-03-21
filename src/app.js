c9695b0a6efe26a48c03564bcf32cbaf381a4198
Vrishin Doddapuneni <doddapuneni@wisc.edu>
2025-03-21 12:58:54 -0500

    changed userpage to format it better

--- src/app.js
+++ src/app.js
@@ -1,40 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import GradeSearch from './components/GradeSearch';
 import CourseDetails from './components/CourseDetails';
 import UserPage from './userpage';
 
 export default function App() {
-    const [courseList, setCourseList] = useState([]);
+  const [courseList, setCourseList] = useState([]);
 
-    // Fetch courses from MadGrades API when component mounts
-    useEffect(() => {
-        fetch("https://api.madgrades.com/v1/courses", {
-            headers: {
-                "Authorization": "Token token=052ae8133724409ba61902593bee5db6"
-            }
-        })
-        .then((res) => res.json())
-        .then(data => {
-            setCourseList(data.results);
-        })
-        .catch(error => console.error("Error fetching courses:", error));
-    }, []);
+  // Fetch courses from MadGrades API when component mounts
+  useEffect(() => {
+    fetch("https://api.madgrades.com/v1/courses", {
+      headers: {
+        "Authorization": "Token token=052ae8133724409ba61902593bee5db6"
+      }
+    })
+      .then((res) => res.json())
+      .then(data => {
+        setCourseList(data.results);
+      })
+      .catch(error => console.error("Error fetching courses:", error));
+  }, []);
 
-    return (
-        <Routes>
-            {/* Homepage with course search and list */}
-            <Route path="/" element={<GradeSearch courseList={courseList} />} />
+  return (
+    <Routes>
+      {/* Redirect the root path to the login page */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      
+      {/* Login page */}
+      <Route path="/login" element={<UserPage />} />
 
-            {/* Course details page */}
-            <Route path="/course/:courseId" element={<CourseDetails />} />
-        </Routes>
-    );
-    return (
-    <div className="App">
-      <UserPage />
-    </div>
+      {/* Main page (after login) */}
+      <Route path="/main" element={<GradeSearch courseList={courseList} />} />
+
+      {/* Course details page */}
+      <Route path="/course/:courseId" element={<CourseDetails />} />
+
+      {/* Catch-all: Redirect any unknown paths to login */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
   );
-
 }
 
