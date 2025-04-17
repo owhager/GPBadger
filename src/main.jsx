1c4a02469315fbac7d1c8ea6a9243dbf11c7d4c6
Aarushi Choudhary <achoudhary22@wisc.edu>
2025-04-16 21:21:38 -0500

    fixing error

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from './app';
 import { UserProvider } from '../src/contexts/UserContext'; // global user state for login/logout
 import 'bootstrap/dist/css/bootstrap.min.css';
 
